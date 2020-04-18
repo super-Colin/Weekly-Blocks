8869e7fe4d2a41c6072bc62e9afd3d355078dab7
superColin <supercolin.dev@gmail.com>
2020-04-18 11:10:25 -0400

    add color mirror idea, line that follows mouse and splits screen colors

--- sidebarMenu/sidebarMenu.js
+++ sidebarMenu/sidebarMenu.js
@@ -2,6 +2,7 @@
 document.getElementById('sidebarMenu-iconsTab-toggleTabButton').addEventListener('click', toggleExpandingSidebar);
 function toggleExpandingSidebar(){
     document.body.classList.toggle('sidebarMenu-expanded');
+    document.getElementById('sidebarMenu-iconsTab-toggleTabButton').classList.toggle('bookshelfToX-icon-active')
 }
 
 
