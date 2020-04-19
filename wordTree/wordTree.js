3a9f21e4c67471e87824e1b8380198a4baf3728e
superColin <supercolin.dev@gmail.com>
2020-04-19 06:40:31 -0400

    trunk is there

--- wordTree/wordTree.js
+++ wordTree/wordTree.js
@@ -1,48 +1,76 @@
     let treeContainer = document.getElementById('wordTree-container');
-    let disposableContainer;
+    let wordTree_disposableContainer;
+    let wordTree_mainWordSpan;
+
+    // let lastAdded = [ 'last added elem', 'rotation', 'transform originX', 'transform originY'];
+    let lastAdded = [wordTree_mainWordSpan];
+    console.log(lastAdded);
+
 generate_wordTree();
 
 function generate_wordTree(){
     let mainWord = 'Unique';
-    let secondWords = ['Determined', 'Another', 'More']
+    let secondWords = ['Determined'];
+    let thirdWords = ['Motivated', 'Another', 'More'];
 
     // create and append disposable container to outer container
-    disposableContainer = document.createElement('div');
-    disposableContainer.classList.add('wordTree-disposableContainer');
-    treeContainer.appendChild(disposableContainer);
+    wordTree_disposableContainer = document.createElement('div');
+    wordTree_disposableContainer.classList.add('wordTree-wordTree_disposableContainer');
+    treeContainer.appendChild(wordTree_disposableContainer);
 
     // create and append main word to disposable container
-    let mainWordSpan = document.createElement('span');
-    mainWordSpan.classList.add('wordTree-words-tier1');
-    mainWordSpan.appendChild(document.createTextNode(mainWord));
-    disposableContainer.appendChild(mainWordSpan);
+    wordTree_mainWordSpan = document.createElement('span');
+    wordTree_mainWordSpan.classList.add('wordTree-words-tier1');
+    wordTree_mainWordSpan.appendChild(document.createTextNode(mainWord));
+    wordTree_disposableContainer.appendChild(wordTree_mainWordSpan);
 
-    mainWordSpan.style.top = (treeContainer.offsetHeight / 2) - (mainWordSpan.offsetHeight / 2) + 'px';
-    mainWordSpan.style.left = (treeContainer.offsetWidth / 2) - (mainWordSpan.offsetWidth / 2) + 'px';
+    wordTree_mainWordSpan.style.top = (treeContainer.offsetHeight / 2) - (wordTree_mainWordSpan.offsetHeight / 2) + 'px';
+    wordTree_mainWordSpan.style.left = (treeContainer.offsetWidth / 2) - (wordTree_mainWordSpan.offsetWidth / 2) + 'px';
 
-    addWords('wordTree-words-tier2', secondWords);
-    
+    addWords(2, secondWords);
+    addWords(3, thirdWords);
+
 }
 
 
-function addWords(elemClass, elemContent, elemParent = disposableContainer) {
+function addWords(wordTier, elemContent, elemParent = wordTree_disposableContainer) {
+    branchLength = 1;
     for(let word of elemContent){
         let newWordSpan = document.createElement('span');
-        newWordSpan.classList.add(elemClass);
-        // newWordSpan.appendChild(document.createTextNode(word));
+        newWordSpan.classList.add('wordTree-words-tier' + wordTier);
         newWordSpan.innerHTML = word;
         elemParent.appendChild(newWordSpan);
 
-        // findWordSpot(newWordSpan);
+        getWordSpot(newWordSpan, wordTier, branchLength);
+        branchLength ++;
     }
 }
 
-// function findWordSpot() {
+// function addWord(elemClass, elemContent, elemParent = wordTree_disposableContainer){
 
 // }
 
-// function getElemDimensions(elem){
-//     return [elem.offsetWidth, elem.offsetHeight];
-// }
+function getWordSpot(elem, branchTier, branchNumber) {
+    if( branchTier === 2 && branchNumber === 1 ){
+        console.log(elem.offsetLeft);
+        elem.style.left = wordTree_mainWordSpan.offsetLeft + (wordTree_mainWordSpan.offsetLeft / 2) + 'px';
+        elem.style.top = (wordTree_mainWordSpan.offsetTop - elem.offsetHeight) + 'px';
+        let rotation = -90;
+        elem.style.transform = 'rotate(' + rotation + 'deg)';
+        lastAdded[1] = rotation;
+        let transformOriginX = 0;
+        let transformOriginY = 100;
+        elem.style.transformOrigin = transformOriginX + '% ' + transformOriginY + '%';
+        lastAdded
+        // elem.style.transformOrigin = ;
+
+        console.log(elem.offsetLeft);
+
+        lastAdded[0] = elem;
+    } else {
+        console.log('else triggered');
+    }
+
+}
 
 
