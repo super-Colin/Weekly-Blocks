c275e99322a5c69c2007e5e194b0dd86843d3bda
superColin <supercolin.dev@gmail.com>
2020-04-22 20:24:40 -0400

    both main branches working, need to adjust positioning randomness

--- wordTree/wordTree.js
+++ wordTree/wordTree.js
@@ -5,15 +5,20 @@ let wordTree_disposableContainer;
 let wordTree_mainWordSpan;
 let wordTree_trunkWordSpan;
 
-// let lastAdded = [ 'last added elem', 'rotation', 'transform originX', 'transform originY'];
+// let lastAdded = [ 'last added elem', 'side of parent branch'];
 let lastAdded = [wordTree_mainWordSpan];
 
 generate_wordTree();
 
 function generate_wordTree(){
+    // Main word, the trunk sprouts from this
     let mainWord = 'Unique';
+    // Trunk of the tree
     let secondWords = ['Determined'];
-    let thirdWords = ['Motivated', 'Another', 'More'];
+    // Branches from the trunk
+    let thirdWords = ['Motivated', 'Another'];
+    // Leaves off of the branches
+    let fourthWords = ['Motivated', 'Another', 'More'];
 
     // create and append disposable container to outer container
     wordTree_disposableContainer = document.createElement('div');
@@ -32,6 +37,7 @@ function generate_wordTree(){
 
     addWords(2, secondWords);
     addWords(3, thirdWords);
+    addWords(4, fourthWords);
 
 }
 
@@ -57,48 +63,71 @@ function addWords(wordTier, elemContent, elemParent = wordTree_disposableContain
 
 function getWordSpot(elem, branchTier, branchNumber) {
     
+    let randomInt = Math.random() / 2 + 0.3;
+    console.log('random int is: ' + randomInt);
+
     // Place and rotate the trunk of the tree
     if( branchTier === 2 && branchNumber === 1 ){
-        
         // Center trunk on top of main word
         elem.style.left = wordTree_mainWordSpan.offsetLeft + (wordTree_mainWordSpan.offsetLeft / 2) + 'px';
         elem.style.top = (wordTree_mainWordSpan.offsetTop - elem.offsetHeight) + 'px';
 
-        // Set transform origin and update last added
-        let transformOriginX = 0;
-        let transformOriginY = 100;
-        elem.style.transformOrigin = transformOriginX + '% ' + transformOriginY + '%';
-        lastAdded[2] = transformOriginX;
-        lastAdded[3] = transformOriginY;
+        // Set transform origin 
+        elem.style.transformOrigin = 0 + '% ' + 100 + '%';
 
-        // Rotate update last added
-        let rotation = -90;
-        elem.style.transform = 'rotate(' + rotation + 'deg)';
-        lastAdded[1] = rotation;
+        // Rotate, -90 deg so that word is vertical on top of main word
+        elem.style.transform = 'rotate(-90deg)';
 
         wordTree_trunkWordSpan = elem;
 
     // Start placing branches on the side of the trunk (set above)
     }else if(branchTier === 3 && branchNumber === 1) {
+        console.log('first main branch')
 
         // go on left side of trunk
         if(Math.random() < 0.5){
             console.log('left');
+            lastAdded[1] = 'left';
             elem.style.left = (parseFloat(wordTree_trunkWordSpan.style.left) - wordTree_trunkWordSpan.offsetHeight) - elem.offsetWidth + 'px';
 
-            elem.style.top = ((parseFloat(wordTree_trunkWordSpan.style.top) + wordTree_trunkWordSpan.offsetHeight) - elem.offsetHeight) - (wordTree_trunkWordSpan.offsetWidth * 0.8) + 'px';
+            elem.style.top = ((parseFloat(wordTree_trunkWordSpan.style.top) + wordTree_trunkWordSpan.offsetHeight) - elem.offsetHeight) - (wordTree_trunkWordSpan.offsetWidth * randomInt) + 'px';
 
 
         // go on right side of trunk
         }else{
             console.log('right');
+            lastAdded[1] = 'right';
             elem.style.left = wordTree_trunkWordSpan.style.left;
 
-            elem.style.top = ((parseFloat(wordTree_trunkWordSpan.style.top) + wordTree_trunkWordSpan.offsetHeight) - elem.offsetHeight) - (wordTree_trunkWordSpan.offsetWidth * 0.8) + 'px';
+            elem.style.top = ((parseFloat(wordTree_trunkWordSpan.style.top) + wordTree_trunkWordSpan.offsetHeight) - elem.offsetHeight) - (wordTree_trunkWordSpan.offsetWidth * randomInt) + 'px';
+        }
 
 
+    // same as above, but make sure this branch is on the other side of the trunk
+    } else if (branchTier === 3 && branchNumber === 2){
+        console.log('next main branch');
+
+
+        // Go on left side of trunk
+        if (lastAdded[1] === 'right') {
+
+            console.log('left');
+            lastAdded[1] = 'left';
+            elem.style.left = (parseFloat(wordTree_trunkWordSpan.style.left) - wordTree_trunkWordSpan.offsetHeight) - elem.offsetWidth + 'px';
+
+            elem.style.top = ((parseFloat(wordTree_trunkWordSpan.style.top) + wordTree_trunkWordSpan.offsetHeight) - elem.offsetHeight) - (wordTree_trunkWordSpan.offsetWidth * randomInt) + 'px';
+
+
+        // go on right side of trunk
+        } else {
+            console.log('right');
+            lastAdded[1] = 'right';
+            elem.style.left = wordTree_trunkWordSpan.style.left;
+
+            elem.style.top = ((parseFloat(wordTree_trunkWordSpan.style.top) + wordTree_trunkWordSpan.offsetHeight) - elem.offsetHeight) - (wordTree_trunkWordSpan.offsetWidth * randomInt) + 'px';
         }
     }
+    
 
 }
 
